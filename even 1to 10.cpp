9e38f1184ebac4b7832d4c73f515fbc0e63ced4d
NagaBabu30 <132924900+NagaBabu30@users.noreply.github.com>
2024-06-25 10:46:08 +0530

    Add files via upload

---
+++ even 1to 10.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "even numbers from 1 to 10:" << endl;
+    for (int i = 2; i <= 10; i += 2) {
+        cout << i << " "<< endl;;
+    }  
+    return 0;
+}
